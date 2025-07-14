8fbeb5529800bb0a5848346874e71e8cffd6c737
Nababmithun <monermithun789@gmail.com>
2025-07-14 20:15:42 +0600

    permission add

--- app/src/main/java/com/example/qasystemappcompose/network/ApiService.kt
+++ app/src/main/java/com/example/qasystemappcompose/network/ApiService.kt
@@ -1,8 +1,9 @@
 package com.example.qasystemappcompose.network
 
 import com.example.qasystemappcompose.model.APIResponse
-import okhttp3.Response
+import retrofit2.Response
 import retrofit2.http.GET
+
 
 interface ApiService {
     @GET("v3/b/687374506063391d31aca23a")
